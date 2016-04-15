9070d49e8a883fdedd6ab3a3fd65b167ad1bb840
jason yang <jason.yang@baluntek.com>
2016-04-15 11:22:24 +0800

    [IMP] 增加表单内容

--- entries/blueprint.py
+++ entries/blueprint.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 from helpers import object_list
 from models import Entry, Tag
+from forms import EntryForm
+from app import db
 
 entries = Blueprint('entries', __name__, template_folder='templates')
 
@@ -20,7 +22,22 @@ def tag_detail(slug):
 	entries = tag.entries.order_by(Entry.created_timestamp.desc())
 	return object_list('entries/tag_detail.html', entries, tag=tag)
 
+@entries.route('/create/', methods=['GET', 'POST'])
+def create():
+	if request.method == 'POST':
+		form = EntryForm(request.form)
+		if form.validate():
+			entry = form.save_entry(Entry())
+			db.session.add(entry)
+			db.session.commit()
+			return redirect(url_for('entries.detail', slug=entry.slug))
+	else:
+		form = EntryForm()
+
+	return render_template('entries/create.html', form=form)
+
 @entries.route('/<slug>/')
 def detail(slug):
 	entry = Entry.query.filter(Entry.slug == slug).first_or_404()
 	return render_template('entries/detail.html', entry=entry)
+
